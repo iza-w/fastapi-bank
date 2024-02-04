ba48c0d7363078e853a40ba5af4ed0c8edb0beee
Izabela Kaczyńska <izabela@devsoft.pl>
2024-02-04 13:18:41 +0100

    feat(api): add tests for SQLAlchemyAccountRepository

--- tests/integration/test_healthcheck.py
+++ tests/integration/test_healthcheck.py
@@ -1,9 +1,7 @@
 from starlette import status
 
 
-async def test_healthcheck__returns_ok(
-    app, async_client
-):
+async def test_healthcheck__returns_ok(app, async_client):
     response = await async_client.get(app.url_path_for("health"))
 
     assert response.status_code == status.HTTP_200_OK
---
+++ tests/unit/test_account_repository.py
@@ -0,0 +1,67 @@
+from decimal import Decimal
+
+import pytest
+
+from app.domain.accounts.account import Account
+from app.domain.accounts.exceptions import AccountDoesNotExist
+from app.infrastructure.respositories.account_repository import SQLAlchemyAccountRepository
+
+
+async def test_get_account(async_session):
+    repository = SQLAlchemyAccountRepository(session=async_session)
+
+    with pytest.raises(AccountDoesNotExist):
+        await repository.get(account_id=1)
+
+
+async def test_get_list__returns_empty_list(async_session):
+    repository = SQLAlchemyAccountRepository(session=async_session)
+
+    accounts = await repository.get_list()
+    assert accounts == []
+
+
+async def test_get_list__returns_expected_accounts(async_session):
+    repository = SQLAlchemyAccountRepository(session=async_session)
+
+    async with async_session.begin():
+        async_session.add(Account(name="Jenny"))
+        async_session.add(Account(name="John", balance=Decimal("10.00")))
+
+    accounts = await repository.get_list()
+
+    assert len(accounts) == 2
+    assert accounts[0].name == "Jenny"
+    assert accounts[1].name == "John"
+    assert accounts[0].balance == Decimal("0.00")
+    assert accounts[1].balance == Decimal("10.00")
+
+
+async def test_add__adds_account_to_database(async_session):
+    repository = SQLAlchemyAccountRepository(session=async_session)
+
+    async with async_session.begin():
+        repository.add(Account(name="Jenny"))
+
+    accounts = await repository.get_list()
+
+    assert len(accounts) == 1
+    assert accounts[0].name == "Jenny"
+    assert accounts[0].balance == Decimal("0.00")
+
+
+async def test_delete__removes_account_from_database(async_session):
+    repository = SQLAlchemyAccountRepository(session=async_session)
+
+    async with async_session.begin():
+        account = Account(name="Jenny")
+        repository.add(account)
+
+    accounts = await repository.get_list()
+    assert len(accounts) == 1
+
+    async with async_session.begin():
+        await repository.delete(account)
+
+    accounts = await repository.get_list()
+    assert len(accounts) == 0
