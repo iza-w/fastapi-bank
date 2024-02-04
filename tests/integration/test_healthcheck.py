78d22d6f93f7a7d3f9d08c15044a9008c28597f7
Izabela Kaczyńska <izabela@devsoft.pl>
2024-02-04 12:59:27 +0100

    feat(api): add tests for healtcheck

---
+++ tests/integration/test_healthcheck.py
@@ -0,0 +1,10 @@
+from starlette import status
+
+
+async def test_healthcheck__returns_ok(
+    app, async_client
+):
+    response = await async_client.get(app.url_path_for("health"))
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == {"status": "ok"}
